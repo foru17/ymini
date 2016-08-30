bcd4a972dcc84ea6faa1709cdada792f86d4fda1
p_leilluo(罗磊) <luolei@yuewen.com>
2016-08-30 21:10:40 +0800

    增加css压缩

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,13 @@ var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var gutil = require('gulp-util');
 var pump = require('pump');
-
+var cleanCSS = require('gulp-clean-css');
 
 gulp.task('scripts', function(cb) {
-	console.log('执行js压缩');
+    console.log('执行js压缩');
     var _progressPash = gutil.env.path ? gutil.env.path : '';
- 	var _progressJsPash = _progressPash + '/**/*.js'
- 	console.log(_progressPash);
+    var _progressJsPash = _progressPash + '/**/*.js'
+    console.log(_progressPash);
     pump([
             gulp.src(_progressJsPash),
             uglify(),
@@ -16,4 +16,14 @@ gulp.task('scripts', function(cb) {
         ],
         cb
     );
+});
+
+
+gulp.task('css', function(cb) {
+    console.log('执行css压缩');
+    var _progressPash = gutil.env.path ? gutil.env.path : '';
+    var _progressCssPash = _progressPash + '/**/*.css'
+    return gulp.src(_progressCssPash)
+        .pipe(cleanCSS({ compatibility: 'ie8' }))
+        .pipe(gulp.dest(_progressPash));
 });
