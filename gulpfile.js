ca3da742aada2ebdc40dace143434bce4a75544e
luolei <i@luolei.org>
2017-09-22 18:39:06 +0800

    优化

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,30 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var gutil = require('gulp-util');
-var pump = require('pump');
+var path = require('path');
 var cleanCSS = require('gulp-clean-css');
 var filesize = require('filesize');
+var filter = require('gulp-filter');
+var plumber = require('gulp-plumber');
 
-gulp.task('scripts', function() {
-    console.log('执行js压缩');
-    var _progressPash = gutil.env.path ? gutil.env.path : '';
-    var _progressJsPash;
-    // 如果是目录
-    if ((_progressPash).lastIndexOf('.js') > -1) {
-        _progressJsPash = _progressPash;
+gulp.task('smart', function() {
+    console.log('[Ymini]执行智能压缩');
+    var progressPash = gutil.env.path ? gutil.env.path : '';
+    var progressFilePash = path.join(progressPash);
+
+    if ((progressPash).lastIndexOf('.js') > -1 || (progressPash).lastIndexOf('.css') > -1) {
+        progressFilePash = progressPash;
     } else {
-        _progressJsPash = _progressPash + '/**/*.js';
+        progressFilePash = path.join(progressFilePash,'**/*');
     }
-    pump([
-            gulp.src(_progressJsPash),
-            uglify({
+    console.log(progressFilePash)
+    var cssFilter = filter(['**/*.css'], {restore: true});
+    var jsFilter = filter(['**/*.js'], {restore: true});
+
+    return gulp.src(progressFilePash)
+        .pipe(plumber())
+        .pipe(jsFilter)
+        .pipe(uglify({
                 mangle: {
                     reserved: ['define', 'require', 'module', 'exports'],
                 },
@@ -25,44 +32,23 @@ gulp.task('scripts', function() {
                     keep_quoted_props:true
                 },
                 ie8: true
-            }).on('error',function(e){
-                console.log('出错了')
-                console.log('[JS文件异常]' + e)
-            }),
-            gulp.dest(function(file) {
-                return file.base
-            })
-        ]
-    );
-});
-
-gulp.task('css', function(cb) {
-    console.log('执行css压缩');
-    var _progressPash = gutil.env.path ? gutil.env.path : '';
-    var _progressCssPash = _progressPash + '/**/*.css';
-    var _progressJsPash;
-
-    // 如果是目录
-    if ((_progressPash).lastIndexOf('.css') > -1) {
-        _progressJsPash = _progressPash;
-    } else {
-        _progressJsPash = _progressPash + '/**/*.css';
-    }
-    pump([
-            gulp.src(_progressJsPash),
-            cleanCSS({
+            }))
+        .pipe(jsFilter.restore)
+        .pipe(cssFilter)
+        .pipe(cleanCSS({
                 debug:true,
                 compatibility: 'ie8',
                 processImport: false,
                 inline:'none'
             }, function(file) {
                 console.log('[压缩CSS]' + file.name + ': [' + filesize(file.stats.originalSize) + ' >> ' + filesize(file.stats.minifiedSize) + ']');
-            }),
-            gulp.dest(function(file) {
-                return file.base
-            })
+        }))
+        .pipe(cssFilter.restore)
+        .pipe(gulp.dest(function(file){
+            return file.base
+        }));
 
-        ],
-        cb
-    );
 });
+gulp.task('default', ['smart']);
+gulp.task('css', ['smart']);
+gulp.task('scripts', ['smart']);
